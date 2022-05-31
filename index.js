5a2cc2f0d7aa3a84d3928187418cf6acf9c0a850
Rajath <acharya.drajath@gmail.com>
2022-05-31 11:32:06 +0530

    feat(server): init

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+const PORT = 5000;
+
+app.use((req, res, next) => {
+  console.log("Request received");
+  console.log(`${req.method} ${req.path}`);
+  res.on("finish", () => {
+    console.log(`${req.method} ${req.path} ${res.statusCode}`);
+  });
+  next();
+});
+
+app.get("/", (req, res) => {
+  fs.readFile("./temp.txt", { encoding: "utf-8" }, (err, text) => {
+    res.send(text);
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).send("Not found");
+});
+
+app.listen(PORT, () => {
+  console.log("listening on PORT ", PORT);
+});

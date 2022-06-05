6948edc930c413c3ef074ce4214f25ee09e6d6e1
Rajath <acharya.drajath@gmail.com>
2022-06-05 20:47:35 +0530

    added .prettierrc config appliend new config styles to index.js

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const express = require("express");
-const fs = require("fs");
+const express = require('express')
+const fs = require('fs')
 
-const app = express();
+const app = express()
 
-const PORT = 5000;
+const PORT = 5000
 
 app.use((req, res, next) => {
-  console.log("Request received");
-  console.log(`${req.method} ${req.ip} ${req.path}`);
-  res.on("finish", () => {
-    console.log(`${req.method} ${req.ip} ${req.path} ${res.statusCode}`);
-  });
-  next();
-});
+  console.log('Request received')
+  console.log(`${req.method} ${req.ip} ${req.path}`)
+  res.on('finish', () => {
+    console.log(`${req.method} ${req.ip} ${req.path} ${res.statusCode}`)
+  })
+  next()
+})
 
-app.get("/", (req, res) => {
-  fs.readFile("./temp.txt", { encoding: "utf-8" }, (err, text) => {
-    res.send(text);
-  });
-});
+app.get('/', (req, res) => {
+  fs.readFile('./temp.txt', { encoding: 'utf-8' }, (err, text) => {
+    res.send(text)
+  })
+})
 
 app.use((req, res) => {
-  res.status(404).send("Not found");
-});
+  res.status(404).send('Not found')
+})
 
 app.listen(PORT, () => {
-  console.log("listening on PORT ", PORT);
-});
+  console.log('listening on PORT ', PORT)
+})
